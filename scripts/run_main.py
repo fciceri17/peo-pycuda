46a0dad36a3d9a0f2f026eb313a749a7608f7666
cesarebernardis <cesare.bernardis@mail.polimi.it>
2017-05-27 12:14:03 +0200

    Created main

--- scripts/run_main.py
+++ scripts/run_main.py
@@ -1,4 +1,7 @@
 import networkx as nx
+import numpy as np
+import math
+
 from peo_pycuda.chordal_gen import generateChordalGraph
 
 N = 50
@@ -6,4 +9,6 @@ DENSITY = 0.5
 
 G = generateChordalGraph(N, DENSITY)
 Gcsr = nx.to_scipy_sparse_matrix(G)
+numbering = np.zeros(N, dtype=np.int)
+delta = 8 ** math.ceil(math.log(N, 5/4))
 print(Gcsr)
